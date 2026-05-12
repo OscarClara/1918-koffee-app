be96fd3f7744c0b0da67aeb51f18cb31bb9b4495
OscarClara <otriana110152@umanizales.edu.co>
2026-05-12 16:35:16 -0500

    Agregando codigo a los archivos de prueba y configurando jest

--- calculadora.test.js
+++ calculadora.test.js
@@ -1,4 +1,4 @@
 const calcularTotal = require('./calculadora');
 test('Debe calcular el total de café correctamente', () => {
-  expect(calcularTotal(25000, 2)).toBe(50000);
+  expect(calcularTotal(30000, 2)).toBe(55000);
 });
